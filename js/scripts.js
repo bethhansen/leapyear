93d4d33b9732713bf6a6abd8e9f7309ee6dfd09d
Beth Hansen and Paul Guevarra <beth@>
2017-07-12 09:26:30 -0700

    add first true statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
+      if (year % 4 === 0) {
+      return true;
+    } else {
       return false;
+      }
     };
+
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
